6ac85ac863e0af3dd190c60069627b5638daebde
wiktorCzechowski8127 <wiktor.czechowski.ti@gmail.com>
2024-02-04 19:55:03 +0100

    Test commit with speeded time function

--- main.py
+++ main.py
@@ -1,79 +1,25 @@
 # -*- coding: utf-8 -*-
 
+##########################################################
+# Imports
+##########################################################
 from excel import *
 from  employees import *
 import datetime
 import os
-import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-
-
-BUZZER = 12
-YELLOW_LED = 20
-GREEN_LED = 21
-
-GPIO.setup(BUZZER, GPIO.OUT)
-GPIO.setup(YELLOW_LED,GPIO.OUT)
-GPIO.setup(GREEN_LED,GPIO.OUT)
-
-GPIO.output(BUZZER,False)
-GPIO.output(YELLOW_LED,False)
-GPIO.output(GREEN_LED,False)
-
+import gpio
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
+
+##########################################################
+# Defines
+##########################################################
+STARTING_WORK_TIME = 7
 COOLDOWN_TIME_IN_SEC = 2
 
-def blinkSuccess(ledPort):
-    GPIO.output(YELLOW_LED, False)
-    GPIO.output(GREEN_LED, False)
-    
-    GPIO.output(ledPort, True)
-    GPIO.output(BUZZER, True)
-    time.sleep(0.05)
-
-    GPIO.output(ledPort, False)
-    GPIO.output(BUZZER, False)
-    time.sleep(0.05)
-
-    GPIO.output(ledPort, True)
-    GPIO.output(BUZZER, True)
-    time.sleep(0.05)
-    
-    GPIO.output(ledPort, False)
-    GPIO.output(BUZZER, False)
-
-def blinkFailiture(ledPort):
-
-    GPIO.output(YELLOW_LED, False)
-    GPIO.output(GREEN_LED, False)
-    
-    GPIO.output(ledPort, True)
-    GPIO.output(BUZZER, True)
-    time.sleep(0.05)
-
-    GPIO.output(ledPort, False)
-    time.sleep(0.05)
-
-    GPIO.output(ledPort, True)
-    time.sleep(0.05)
-    
-    GPIO.output(ledPort, False)
-    time.sleep(0.05)
-
-    GPIO.output(ledPort, True)
-    time.sleep(0.05)
-    
-    GPIO.output(ledPort, False)
-    time.sleep(0.05)
-    
-    GPIO.output(BUZZER, False)
-
-def readingTokenLed():
-    GPIO.output(YELLOW_LED, True)
-    GPIO.output(GREEN_LED, False)
-    
+##########################################################
+# Functions
+##########################################################
 def changeFile(employees, exc, now, currentExcelFile):   
     excToCopy = excelSheetC(currentExcelFile)
     excToCopy.create_workbook()
@@ -92,24 +38,29 @@ def defineMonthAndFile():
 
 def checkMonthAndFile(employees, exc, now, currentMonth, isSafeMode):
     if (now.month != currentMonth):
-        print("Changing month")
+        gpio.excelProcessed()
         isSafeMode = True
         employeesAfterUpdate = employeesC()
+        lastExcelFile = exc.filename
         if(employeesAfterUpdate.updateEmployeesStatus(exc)):
             if(employeesAfterUpdate.addNewEmployees(exc, now) == 0):
                 employeesAfterUpdate.deleteLayedEmployees()
                 employees = employeesAfterUpdate 
-
                 currentExcelFile = str(now.month) + "_" + str(now.year) + ".xlsx"
                 exc = changeFile(employees, exc, now, currentExcelFile)
+                try:
+                    os.replace(DIRECTORY + lastExcelFile, DIRECTORY + ARCHIVE + lastExcelFile)
+                except Exception as e: 
+                    pass
                 currentMonth = now.month
                 isSafeMode = False
             else:
-                print("Theres still new employees in sheet.")
                 logger.error("Theres still new employees in sheet.")
     return employees, exc, currentMonth, isSafeMode
 
+
 def updateEmployees(employees, exc, now):
+    gpio.excelProcessed()
     print("Updating Employees")
     isSafeMode = True
     employeesAfterUpdate = employeesC()
@@ -121,13 +72,14 @@ def updateEmployees(employees, exc, now):
     exc.lastModificationTime = time.ctime(os.path.getmtime(exc.file))
     return employees, exc, isSafeMode
 
-
+##########################################################
+# Main
+##########################################################
 if __name__=="__main__": 
-    logger.error("Program start running...")   
-    #currentExcelFile = str(now.month) + "_" + str(now.year) + ".xlsx"
-    #currentExcelFile, currentMonth = defineMonthAndFile()
-    isSafeMode = False
-    #currentExcelFile = '2_2024.xlsx'
+
+    ########################################################################
+    #TEST
+        #currentExcelFile = '2_2024.xlsx'
     NOWYEAR = 2024
     NOWMONTH = 2
     NOWDAY = 1
@@ -135,11 +87,24 @@ if __name__=="__main__":
     NOWMINUTES = 0
     NOWSECONDS = 0
     now = datetime.datetime(NOWYEAR, NOWMONTH, NOWDAY, NOWHOUR, NOWMINUTES, NOWSECONDS)
-    lastMinute = 0
-    currentMonth = now.month
+    ####################################################################
+    
+    # starting
+    logger.error("Program start running...")
+    gpio.gpioInitialize()
+    isSafeMode = False
 
+    # Time settings
+    #now = datetime.datetime.now() TEST
+    currentMonth = now.month
+    lastSecond = 0
+
+    # Generating objects
     employees = employeesC()
-    exc = excelSheetC(str(now.month) + "_" + str(now.year) + ".xlsx")
+    file = str(now.month) + "_" + str(now.year) + ".xlsx"
+    exc = excelSheetC(file)
+
+    # Generating template if file dosn't exist
     exc.checkFileAndInitialize()
     if(not(exc.isFileInitializeCorrectly)):
         isSafeMode = True
@@ -150,17 +115,17 @@ if __name__=="__main__":
         excTmp.saveSheet()
         del(excTmp)
 
-
-    i = 0
-    #updateEmployees(exc, now)
+    #setting steering parametes
     lastReadedId = 0
     lastReadedTimeStamp = 0
     coolDown = 0
     signalLed = True
-    signalLedPort = GREEN_LED
-    unusedSignalLedPort = YELLOW_LED
-    while(True):
 
+    # main loop
+    while(True):
+        # setting current time
+        #now = datetime.datetime.now()
+        # checking month and upating file if theres any changes
         if(os.path.isfile(exc.file)):
             employees, exc, currentMonth, isSafeMode = \
                 checkMonthAndFile(employees, exc, now, currentMonth, isSafeMode)
@@ -168,65 +133,65 @@ if __name__=="__main__":
             if (exc.lastModificationTime != modificationTimeSample):
                 employees, exc, isSafeMode = updateEmployees(employees, exc, now)
 
-        if (isSafeMode):
-            signalLedPort = YELLOW_LED
-            unusedSignalLedPort = GREEN_LED
-        else:
-            signalLedPort = GREEN_LED
-            unusedSignalLedPort = YELLOW_LED
-
+        # reading token
         readedTokenId = reader.read_no_block()[0]
+        # processing token
         if ((readedTokenId != None) and (readedTokenId != lastReadedId)):
-            #Led steering
-            readingTokenLed()
+            gpio.readingTokenLed()
             
+            # setting cooldown
             lastReadedTimeStamp = time.time()
             lastReadedId = readedTokenId
 
-            currentTime = now #datetime.datetime.now()
+            # changing time to 7 is its earlier
+            if (now.hour != (STARTING_WORK_TIME - 1)):
+                timeToPasteInExcel = now
+            else:
+                timeToPasteInExcel = \
+                    now.replace(hour = STARTING_WORK_TIME, minute=0, second= 0, microsecond= 0) 
+
+            # putting token in excel 
             if (isSafeMode):
-                logger.error("READED TOKENID: " + str(readedTokenId) + " at time: " + str(currentTime))
-                blinkSuccess(GREEN_LED)
+                logger.error("READED TOKENID: " + str(readedTokenId) + " at time: " + str(now))
+                gpio.blinkSuccess()
             else:
                 employeeId = employees.checkReadedId(readedTokenId)
                 if (employeeId != None):
-                    exc.inputTimestampIntoExcel(readedTokenId, employeeId, currentTime)
-                    blinkSuccess(GREEN_LED)
+                    exc.inputTimestampIntoExcel(readedTokenId, employeeId, timeToPasteInExcel)
+                    gpio.blinkSuccess()
                 else:
                     exc.inputTokenIdToExce(readedTokenId)
-                    blinkFailiture(YELLOW_LED)
-            
-            print("LastReadedId: %d" %lastReadedId)
+                    gpio.blinkFailiture()
+        # taking of cooldown
         coolDown = abs(time.time() - lastReadedTimeStamp)
-
         if (coolDown > COOLDOWN_TIME_IN_SEC):
             lastReadedId = 0
         
+        # blink
+        
 
-        #To Delete   
         now2 = datetime.datetime.now()
-        if (NOWMINUTES == 60):
-            NOWMINUTES = 0
-            NOWHOUR += 1
-            if (NOWHOUR == 24):
+        now = datetime.datetime(NOWYEAR, NOWMONTH, NOWDAY, NOWHOUR, NOWMINUTES, now2.second)
+
+        #signalLed, lastSecond = gpio.blink(now, lastSecond, isSafeMode, signalLed)
+        #TEST 
+        if(now2.second != lastSecond):
+            lastSecond = now.second
+            NOWDAY +=1
+            if (NOWMINUTES == 60):
+                NOWMINUTES = 0
+                NOWHOUR += 1
+            if (NOWHOUR >= 24):
                 NOWHOUR = 1
                 NOWDAY += 1
-        if(NOWDAY > 27):
-            NOWDAY = 1
-            NOWMONTH += 1
-        
+            if(NOWDAY > 27):
+                NOWDAY = 1
+                NOWMONTH += 1
+                    
             if(NOWMONTH > 12):
                 NOWMONTH = 1
                 NOWYEAR +=1
-        now = datetime.datetime(NOWYEAR, NOWMONTH, NOWDAY, NOWHOUR, NOWMINUTES, NOWSECONDS)
+
+            print(str(now))
+
         
-        if(now2.second != lastMinute):
-            tmptext = ''
-            if(isSafeMode):
-                tmptext = ' SafeMode enabled'
-            print(str(now) + tmptext)
-            lastMinute = now2.second
-            NOWMINUTES += 15
-            signalLed = not(signalLed)
-            GPIO.output(signalLedPort, signalLed)
-            GPIO.output(unusedSignalLedPort, False)
